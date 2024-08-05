77b6f7118e95ac8e9eab47767372a17b6f7d0f0e
vitthalpatil-1 <vitthalpatil0019@gmail.com>
2024-08-05 22:00:21 +0530

    Add files via upload

---
+++ Project-1/src/test/java/com/jfs/project/Project1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfs.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
